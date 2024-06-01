7057b80aad7401c3075342d94a113391c3d430cf
Jevaughn Smith <smithjevaughn20@gmail.com>
2024-05-31 19:23:39 -0500

    Added required endpoints

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+app.set('trust proxy', true);
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -20,8 +22,18 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  const clientInfo = {
+    ipaddress: ipAddress,
+    language,
+    software
+  }
+
+  res.json(clientInfo);
 });
 
 // listen for requests :)
